f5d360fd892dceb62a6b460ec7c021394e389142
nelsonmonteiro85 <nelson-monteiro85@hotmail.com>
2025-01-10 10:36:22 +0000

    Footer fixed - page well displayed

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ const app = express()
 const static = require("./routes/static")
 
 /* ***********************
-* Index route
-*************************/
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
-
-/* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+* Index route
+*************************/
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Routes
