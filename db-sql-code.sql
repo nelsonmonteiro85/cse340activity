8b642da2ea39a3ff79e99731f789846cdbcbb70a
nelsonmonteiro85 <nelson-monteiro85@hotmail.com>
2025-01-23 00:09:27 +0000

    Database account_type updated

--- db-sql-code.sql
+++
@@ -1,9 +0,0 @@
--- Type: account_type
-
--- DROP TYPE IF EXISTS public.account_type;
-
-CREATE TYPE public.account_type AS ENUM
-    ('Client', 'Employee', 'Admin');
-
-ALTER TYPE public.account_type
-    OWNER TO cse340nm;
